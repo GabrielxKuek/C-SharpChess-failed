97fc828cbfc03b9fdf007639e0fb7b604e554dd3
Gabriel <139882011+GabrielxKuek@users.noreply.github.com>
2024-03-11 23:47:37 +0800

    Merging from Master branch to Main branch
    too many changes to allow a pull request =(

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    // stores the players' turn, who wins the game and colour of chess pieces
+    public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            return player switch
+            {
+                Player.White => Player.Black, // case 1:
+                Player.Black => Player.White, // case 2:
+                _ => Player.None, // default:
+            };
+        }
+    }
+}
